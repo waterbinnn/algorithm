d1637a9ca2dae4c33b105a49e33ec759a51bb29d
Subin Lee <ewaterbinn@gmail.com>
2025-03-07 15:03:10 +0900

    solve: 2차원 배열 만들기 문제 풀이

---
+++ 코딩테스트 입문 /조건문,배열,수학,시뮬레이션/2차원으로만들기.js
@@ -0,0 +1,33 @@
+/**
+ * @description
+ * 정수 배열 num_list와 정수 n이 매개변수로 주어집니다. num_list를 다음 설명과 같이 2차원 배열로 바꿔 return하도록 solution 함수를 완성해주세요.
+ * num_list가 [1, 2, 3, 4, 5, 6, 7, 8] 로 길이가 8이고 n이 2이므로 num_list를 2 * 4 배열로 다음과 같이 변경합니다.
+ * 2차원으로 바꿀 때에는 num_list의 원소들을 앞에서부터 n개씩 나눠 2차원 배열로 변경합니다.
+ *
+ * @example
+ * [1, 2, 3, 4, 5, 6, 7, 8], 2 => [[1, 2], [3, 4], [5, 6], [7, 8]]
+ * [100, 95, 2, 4, 5, 6, 18, 33, 948], 3 => [[100, 95, 2], [4, 5, 6], [18, 33, 948]]
+ */
+
+function solution(num_list, n) {
+  let ans = [];
+
+  for (let i = 0; i < num_list.length; i += n) {
+    ans.push(num_list.slice(i, i + n));
+  }
+
+  return ans;
+}
+
+solution([1, 2, 3, 4, 5, 6, 7, 8], 2);
+solution([100, 95, 2, 4, 5, 6, 18, 33, 948], 3);
+
+function solution2(num_list, n) {
+  const answer = Array(num_list.length / n)
+    .fill([])
+    .map(() => num_list.splice(0, n));
+
+  return answer;
+}
+
+solution2([100, 95, 2, 4, 5, 6, 18, 33, 948], 3);
